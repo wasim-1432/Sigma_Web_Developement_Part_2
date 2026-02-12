df5cd6e3cf32a69ef1b12e014705a653163c1c46
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-12 22:01:10 +0530

    Hello.

---
+++ JS110/script.js
@@ -0,0 +1,14 @@
+const sum=(a,b)=> {
+    return a+b;
+}
+console.log(sum(2,3));
+
+const cube=(n)=> {
+    return n*n*n;
+}
+console.log(cube(3));
+
+const pow = (a,b) => {
+    return a**b;
+}
+console.log(pow(2,3));
