a58a5d50f5fa7af68cef9edb20a5708ec6a9fbd0
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-12 13:53:44 +0530

    Hello.

---
+++ Js96/script.js
@@ -0,0 +1,32 @@
+function hello(){
+    console.log("Hello World!");
+}
+
+function PrintName()
+{
+    console.log("Apna College");
+}
+
+function Print1to5()
+{
+    for(let i=1;i<=5;i++)
+    {
+        console.log(i);
+    }
+}
+
+function isAdult(){
+    let age=18;
+    if(age>=18)
+    {
+        console.log("Adult");
+    }
+    else
+    {
+        console.log("Not Adult");
+    }
+}
+hello();
+PrintName()
+Print1to5();
+isAdult();
