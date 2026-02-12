a36504211a267492e9bfb3086d0d278653566b7e
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-12 22:52:18 +0530

    Hello.

---
+++ JS115/script.js
@@ -0,0 +1,17 @@
+// let arr=[1,2,3,2,5];
+// function getAvg(arr)
+// {
+//     let sum=0;
+//     for(let i=0;i<arr.length;i++)
+//     {
+//         sum+=arr[i];
+//     }
+//     return sum/arr.length;
+// }
+// console.log(`Average is ${getAvg(arr)}`);
+
+const isEven=(n) =>
+{
+    return n%2==0;
+}
+console.log(isEven(4));
