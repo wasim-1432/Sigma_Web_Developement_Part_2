23771fbbfe54d202ab599ee396032653a1a1661e
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-12 21:49:23 +0530

    Hello.

---
+++ JS109/script.js
@@ -0,0 +1,10 @@
+console.log("Hello");
+console.log("Hello");
+try {
+    console.log(a);
+}catch {
+    console.log("Error");
+}
+console.log("Hello2");
+console.log("Hello2");
+console.log("Hello2");
