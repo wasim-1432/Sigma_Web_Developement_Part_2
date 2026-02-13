d4c37e9faa67f3a9442b7df4137a6dd78d213b57
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-13 17:27:13 +0530

    Hello.

---
+++ JS132/app.js
@@ -0,0 +1,6 @@
+let smallImeges=document.getElementsByClassName("oldImg");
+for(let i=0;i<smallImeges.length;i++)
+{
+    smallImeges[i].src="spiderman_img.png";
+    console.dir(`Value of image no. ${i} is changed.`);
+}
