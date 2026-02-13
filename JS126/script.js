54448230f8aeebad3c10c85f7407e209d16e10b6
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-13 11:15:10 +0530

    Hello.

---
+++ JS126/script.js
@@ -0,0 +1,14 @@
+// let data={
+//     email:"wasim.ahmad0661@gmail.com",
+//     password:"123swegd"
+// }
+// let dataCopy={...data,id:231};
+// console.log(dataCopy);
+
+let arr=[3,2,5,6,9,1,22];
+let arrCopy={...arr};
+console.log(arrCopy);
+
+let chars="apnacollege";
+let charscopy={...chars};
+console.log(charscopy);
