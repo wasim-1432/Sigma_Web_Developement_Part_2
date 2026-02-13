2e21d29935aa088cf1736eccc15a07119af00edd
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-13 18:11:16 +0530

    Hello.

---
+++ JS134/app.js
@@ -0,0 +1,6 @@
+// console.dir(document.querySelector("h1"));
+// console.dir(document.querySelector("#description"));
+// console.dir(document.querySelector(".oldImg"));
+
+console.dir(document.querySelectorAll("div a"));
+console.dir(document.querySelectorAll("p"));
