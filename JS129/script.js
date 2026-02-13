5d8391f3218ad75bf86e1613d357b223bf5a6f51
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-13 11:45:39 +0530

    Hello.

---
+++ JS129/script.js
@@ -0,0 +1,9 @@
+const student={
+    name:"Karan",
+    age:23,
+    class:"Btech",
+    subject:["Physics","chemistry","Math","English"],
+    username:"karan@123",
+    password:"xyz5661"
+};
+let {username,password}=student;
