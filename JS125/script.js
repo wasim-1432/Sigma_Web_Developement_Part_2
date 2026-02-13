918bfa43b7d793b5006b8352af15dde86cb2a11e
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-13 11:05:29 +0530

    Hello.

---
+++ JS125/script.js
@@ -0,0 +1,11 @@
+let arr=[1,2,3,4,5];
+let newArr=[...arr];
+console.log(newArr);
+
+let chars=[..."ApnaCollege"];
+console.log(chars);
+
+let odd=[1,3,5,7];
+let even=[2,4,6,8];
+let arr1=[...odd,...even];
+console.log(arr1);
