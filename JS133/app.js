2951031a6c3e736fe08e0132a65c619e9ecc3868
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-13 17:34:32 +0530

    hello.

---
+++ JS133/app.js
@@ -0,0 +1,8 @@
+// let smallImeges=document.getElementsByClassName("oldImg");
+// for(let i=0;i<smallImeges.length;i++)
+// {
+//     smallImeges[i].src="spiderman_img.png";
+//     console.dir(`Value of image no. ${i} is changed.`);
+// }
+
+document.getElementsByTagName("span")
