5b4016eccd62ba57cf235969125205fe0f71fc0e
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-13 08:12:42 +0530

    Hello.

---
+++ JS118/script.js
@@ -0,0 +1,34 @@
+// let num=[1,2,3,4];
+// let double=num.map((el) => {
+//     return el*2;
+// });
+
+
+// let students=[
+//     {
+//         name:"Mohd Wasim",
+//         marks:99.3
+//     },{
+//         name:"Mohd Namdim",
+//         marks:96.6
+//     },{
+//         name:"Mohd fahim",
+//         marks:98.7
+//     }
+// ];
+
+// let gpa=students.map((el) => {
+//     return el.marks/10;
+// });
+
+// let nums=[1,2,3,4,5,6];
+// let even=nums.filter((el) =>{
+//     return el%2==0;
+// });
+// console.log(even);
+
+let nums=[1,2,3,4,5,6];
+let even=nums.filter((el) =>{
+    return el<5;
+});
+console.log(even);
