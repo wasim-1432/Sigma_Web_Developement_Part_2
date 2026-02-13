c2d741bec96c9ab344dd1de66180c2a68ed47a8c
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-13 07:57:45 +0530

    Hello.

---
+++ JS117/script.js
@@ -0,0 +1,18 @@
+let arr=[1,2,3,4,5];
+let print=function(el) {
+    console.log(el);
+}
+arr.forEach(print);
+
+let arr1=[{
+    name:"Mohd Wasim",
+    marks:67},
+{
+    name:"Mohd Nadim",
+    marks:99}
+];
+
+let print1=function(el) {
+    console.log(el);
+}
+arr1.forEach(print1);
