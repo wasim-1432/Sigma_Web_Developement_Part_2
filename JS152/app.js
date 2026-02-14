dca8f7f4c8b2267642222e65da9eb3a138c8bad2
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-14 22:06:29 +0530

    Hello.

---
+++ JS152/app.js
@@ -0,0 +1,12 @@
+function Hello() {
+    console.log("Inside hello funx");
+    console.log("Hello");
+}
+
+
+function demo() {
+    console.log("Calling hello funx");
+    Hello();
+}
+console.log("Calling demo funx");
+demo();
